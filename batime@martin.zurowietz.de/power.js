d2218d5f3b4022a41656e9d62038b95af5c1caac
Martin Zurowietz <martin@zurowietz.de>
2019-03-17 11:31:00 +0100

    Fix extension for GNOME 3.32
    References #2

--- batime@martin.zurowietz.de/power.js
+++ batime@martin.zurowietz.de/power.js
@@ -1,11 +1,8 @@
 const Lang = imports.lang;
 const UPower = imports.gi.UPowerGlib;
+const BaseIndicator = imports.ui.status.power.Indicator;
 
-var Indicator = new Lang.Class({
-   Name: 'PowerIndicator',
-
-   Extends: imports.ui.status.power.Indicator,
-
+var Indicator = class extends BaseIndicator {
    // Adapted from _getStatus of the parent.
    _getTime() {
       let seconds = 0;
@@ -33,10 +30,10 @@ var Indicator = new Lang.Class({
 
       // Translators: this is <hours>:<minutes>
       return _("%d\u2236%02d").format(hours, minutes);
-   },
+   }
 
    _sync() {
-      this.parent();
+      super._sync();
       this._percentageLabel.clutter_text.set_markup('<span size="smaller">' + this._getTime() + '</span>');
-   },
-});
+   }
+}
