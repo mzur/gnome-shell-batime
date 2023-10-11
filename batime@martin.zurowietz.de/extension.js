fd0b06f3bf056c8324cc5f88979fc18ed037a3d7
Martin Zurowietz <martin@zurowietz.de>
2023-10-11 21:34:13 +0200

    Rewrite the extension for GNOME Shell 45

--- batime@martin.zurowietz.de/extension.js
+++ batime@martin.zurowietz.de/extension.js
@@ -1,33 +1,67 @@
-const ExtensionUtils = imports.misc.extensionUtils;
-const BaTime = ExtensionUtils.getCurrentExtension();
-const Panel = imports.ui.main.panel;
+import {
+   Extension as BaseExtension,
+   InjectionManager
+} from 'resource:///org/gnome/shell/extensions/extension.js';// import {overrideProto} from './util.js'
+import {panel} from 'resource:///org/gnome/shell/ui/main.js';
+import {Indicator} from 'resource:///org/gnome/shell/ui/status/system.js';
+import UPower from 'gi://UPowerGlib';
 
-class BaTimeExtension {
-   constructor() {
-      this.aggregateMenu = Panel.statusArea.quickSettings;
-      this.originalIndicator = this.aggregateMenu._system;
-      this.aggregateMenu._batteryTime = this.customIndicator = new BaTime.imports.power.Indicator();
-      this.aggregateMenu._indicators.replace_child(
-         this.originalIndicator,
-         this.customIndicator
-      );
+const _powerToggleSyncOverride = function () {
+   // Do we have batteries or a UPS?
+   this.visible = this._proxy.IsPresent;
+   if (!this.visible) {
+      return false;
    }
-   destroy() {
-      delete this.aggregateMenu._batteryTime;
-      this.aggregateMenu._indicators.replace_child(
-         this.customIndicator,
-         this.originalIndicator
-      );
+
+   let seconds = 0;
+   let state = this._proxy.State;
+
+   if (this._proxy.State === UPower.DeviceState.CHARGING) {
+      seconds = this._proxy.TimeToFull;
+   } else if (this._proxy.State === UPower.DeviceState.DISCHARGING) {
+      seconds = this._proxy.TimeToEmpty;
    }
-}
 
-let baTime;
+   // This can happen in various cases.
+   if (seconds === 0) {
+      return false;
+   }
 
-function enable() {
-   baTime = new BaTimeExtension();
-}
+   let time = Math.round(seconds / 60);
+   let minutes = time % 60;
+   let hours = Math.floor(time / 60);
 
-function disable() {
-   baTime.destroy();
-   baTime = null;
+   this.title = _('%d\u2236%02d').format(hours, minutes)
+
+   return true;
+};
+
+export default class Extension extends BaseExtension {
+   constructor(metadata) {
+      super(metadata);
+      this._im = new InjectionManager();
+    }
+
+   enable() {
+      this._im.overrideMethod(Indicator.prototype, '_sync', function (_sync) {
+         return function () {
+            const {powerToggle} = this._systemItem;
+            const hasOverride = _powerToggleSyncOverride.call(powerToggle);
+            _sync.call(this);
+            this._percentageLabel.visible = hasOverride;
+         };
+      });
+
+      // This is called if the extension is enabled after startup.
+      // During startup, the _system indicator is not created at this point, yet.
+      if (panel.statusArea?.quickSettings?._system?._systemItem?.powerToggle) {
+         panel.statusArea.quickSettings._system._systemItem.powerToggle._sync();
+      }
+   }
+   disable() {
+      this._im.clear();
+      if (panel.statusArea?.quickSettings?._system?._systemItem?.powerToggle) {
+         panel.statusArea.quickSettings._system._systemItem.powerToggle._sync();
+      }
+   }
 }
